32f5f315aecfa8eb95549fdfef992a218248e621
Mengyuan Liu <liumengyuan23@gmail.com>
2024-03-30 17:31:51 -0700

    change UI and add group Icon (beautiful now)

--- app/src/main/java/edu/northeastern/group18_finalproject/MainActivity.java
+++ app/src/main/java/edu/northeastern/group18_finalproject/MainActivity.java
@@ -3,9 +3,12 @@ package edu.northeastern.group18_finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,8 +17,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button loginButton = findViewById(R.id.loginButton);
-        loginButton.setOnClickListener(new View.OnClickListener(){
+        MaterialButton logInButton = findViewById(R.id.loginButton);
+        logInButton.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.iconBlue)));
+        logInButton.setOnClickListener(new View.OnClickListener(){
+
             @Override
             public void onClick(View view){
                 Intent intent = new Intent(MainActivity.this, ShakeActivity.class);
--- app/src/main/java/edu/northeastern/group18_finalproject/ShakeActivity.java
+++ app/src/main/java/edu/northeastern/group18_finalproject/ShakeActivity.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import java.util.List;
@@ -45,7 +46,7 @@ public class ShakeActivity extends AppCompatActivity {
 
 
 
-        Button uploadRecipeButton = findViewById(R.id.uploadRecipeButton);
+        ImageButton uploadRecipeButton = findViewById(R.id.uploadRecipeButton);
         uploadRecipeButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
